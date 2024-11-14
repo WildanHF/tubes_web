6adb93fba3dd6a07ed9e27bd20fb6aa1729f5645
CrossFode <149864968+CrossFode@users.noreply.github.com>
2024-11-14 20:26:51 +0700

    updateojan

---
+++ JS/donateOpsi.js
@@ -0,0 +1,9 @@
+function showMoneyOptions() {
+    document.getElementById('money-options').style.display = '';
+    document.getElementById('food-form').style.display = 'none';
+}
+
+function showFoodForm() {
+    document.getElementById('food-form').style.display = 'block';
+    document.getElementById('money-options').style.display = 'none';
+}
